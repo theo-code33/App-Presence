ccd0e600d805aca10f472c302048d5975d01bfea
Louis <ls.bouet@gmail.com>
2022-02-01 13:57:04 +0100

    resolve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { ButtonComponent } from './button/button.component';
-=======
-import { ConnexionComponent } from './connexion/connexion.component';
->>>>>>> 19da4da30d69312836e97e6efa6f508b995aad52
+// import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ButtonComponent
-=======
-    ConnexionComponent
->>>>>>> 19da4da30d69312836e97e6efa6f508b995aad52
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,

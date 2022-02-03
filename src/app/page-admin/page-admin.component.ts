624db3e7e6d252dc141e14eab967f4e54d3e4f37
Louis <ls.bouet@gmail.com>
2022-02-03 16:48:06 +0100

    add pdf

--- src/app/page-admin/page-admin.component.ts
+++ src/app/page-admin/page-admin.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { jsPDF } from 'jspdf';
+import  { htmlToText}  from 'html-to-text'
 
 @Component({
   selector: 'app-page-admin',
@@ -10,8 +12,10 @@ import { Router } from '@angular/router';
 export class PageAdminComponent implements OnInit {
 
   nameStudent!:any
+  idStudent!:any
+  table!:any
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router ) { }
 
   ngOnInit(): void {
     this.http.get('http://localhost:3000/students').subscribe(data=>{
@@ -20,5 +24,29 @@ export class PageAdminComponent implements OnInit {
       this.router.navigateByUrl("/admin")
     })
   }
+  returnTo(){
+    this.router.navigateByUrl("/connexion")
+  }
 
+  downloadPdf(){
+    let doc = new jsPDF({});
+    console.log(this.nameStudent)
+    let indexX = 50
+    let indexY = 50
+    let indexX2 = 120
+    doc.setTextColor('#7B61FF')
+    doc.text('Les élèves présents dans votre cours : ', 50, 15)
+    doc.text('Prénom :',50, 40)
+    doc.text('Nom :', 120, 40)
+    this.nameStudent.forEach((name:any)=> {
+      console.log(name)
+      doc.setTextColor('#000')
+      doc.text(name.prenom, indexX, indexY)
+      doc.text(name.nom, indexX2, indexY)
+      indexY += 10
+    });
+
+    doc.save('studentTable.pdf')
+  }
 }
+
